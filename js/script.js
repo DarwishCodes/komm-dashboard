7b9429a8d3ce5594cf6034b7105a0c091316f19d
Muhammad Darwish <mrfunnym007@gmail.com>
2024-05-02 22:33:12 +0300

    fix js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ onresize = (event) => {
 };
 function dynamicHeader() {
   let headerHeight = document.querySelector(".header__wrap").offsetHeight;
-  document.querySelector("main").style.paddingTop = headerHeight + "px";
+  let toolbarHeight = document.querySelector(".tool-bar").offsetHeight;
+
+  document.querySelector("main").style.paddingTop = toolbarHeight + "px";
   document.querySelector("main").style.paddingBottom = headerHeight + "px";
 }
 dynamicHeader();
